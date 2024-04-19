6b20ad8d5fe56ccd03bd13ab94a285c3ae14054d
SakshiSinha <sakshisinha202326@gmail.com>
2024-04-19 16:57:46 +0530

    First Commit

---
+++ asset/js/script.js
@@ -0,0 +1,11 @@
+function Brouchure(){
+    alert("File the Form in the bottom with personal Details to get BROUCHURE");
+}
+
+function redirectyoaboutpage(){
+    window.open('about.html');
+}
+
+function Knowmore(){
+    window.open('contact.html');
+} 
